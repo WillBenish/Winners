48f9659fabf89394cf36b74391f9d912225dffb5
Will Benish <will.benish@gmail.com>
2019-04-21 15:26:12 -0400

    Takes optional player id as parameter.  Added nodemon to package (listen to changes on filesstem, re-start node server automatically)

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,16 +1,35 @@
 var mongoose = require("mongoose");
 var Player = require("../db/models");
 var _ = require("underscore");
+var fs = require("fs");
+var path = require('path');
 
 
 
 mongoose.Promise = global.Promise;
 
 var router = require("express").Router();
-router.route("/player/:id?").get(returnDefaultResponse).post(returnDefaultResponse).delete(returnDefaultResponse).put(returnDefaultResponse);
+router.route("/player/:id?").get(returnPlayers).post(returnDefaultResponse).delete(returnDefaultResponse).put(returnDefaultResponse);
 router.route("/game/:id?").get(returnDefaultResponse).post(returnDefaultResponse).delete(returnDefaultResponse).put(returnDefaultResponse);
 router.route("/gameType/:id?").get(returnDefaultResponse).post(returnDefaultResponse).delete(returnDefaultResponse).put(returnDefaultResponse);
 
+
+function returnPlayers(req,res){
+		let rawdata = fs.readFileSync(__dirname + '/samplePlayers.json');  
+		let players = JSON.parse(rawdata); 
+console.log('boob');
+			console.log('player endpoint get: ');
+			console.log(req.params);
+		if(req.params.id===undefined){
+			res.json(players.samplePlayers); 
+		} else {
+			var objectFound = players.samplePlayers.filter(k => k.id == req.params.id);//map(function(x) {return x.id; }).indexOf('2');
+			res.json(objectFound);
+
+
+		};
+
+}
 
 
 function returnDefaultResponse(req, res){
